c892934b9494d06ff8bd3139daa4f2b781e7540a
Frovy <sfrovis@gmail.com>
2020-09-13 00:19:48 +0300

    p4

---
+++ prac4/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author("Ivan", "ivan@example.com", 'm');
+        System.out.println(author.toString());
+    }
+}
