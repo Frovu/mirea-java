9c20eb3c8271c32079e8741443c1c7fb3b2c82b3
Frovy <sfrovis@gmail.com>
2020-09-12 18:50:26 +0300

    p1: 3,4,5

--- prac1/Main.java
+++ prac1/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Practic 1\nTask 3 (sum array)");
+        int[] array = {1, 3, 13, 23, 5};
+        int sum = 0;
+        int i = 0;
+        for(i=0; i < array.length; ++i)
+            sum += array[i];
+        System.out.println("sum = " + sum);
+
+        sum = 0; i = 0;
+        while(i < array.length) {
+            sum += array[i];
+            ++i;
+        }
+        System.out.println("sum = " + sum);
+
+        sum = 0; i = 0;
+        do {
+            sum += array[i];
+            ++i;
+        } while(i < array.length);
+        System.out.println("sum = " + sum);
+
+
+        System.out.println("\nTask 4 (list command line args)");
+        for(i=0; i < args.length; ++i)
+            System.out.println(args[i]);
+
+
+        System.out.println("\nTask 5 (harmonic row)");
+        for (i = 1; i <= 10; ++i)
+            System.out.print(String.format("%.2f", 1.0 / i) + " ");
+        System.out.println();
     }
 }
